77c7f1a3f8ebba69e1821b0cad989a77c9241340
Damilola-2 <adedahmek00@gmail.com>
2023-08-10 08:27:50 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Unix Entry
- *
- * Return: 1 (success)
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

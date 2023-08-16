878a02371d44dbe6186b3a9f7b0d4af02de5d516
Damilola-2 <adedahmek00@gmail.com>
2023-08-16 03:05:31 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,28 @@
-#include "main.h"
+#include"main.h"
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
- * purpose - no hardcore
+ * main - Entry point
  * Return: 0
  */
+
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 100000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	int a = 1, b = 2, c;
+	int count = 2; /* count of generated fibonacci numbers*/
 
-	printf("%lu", bef);
+	printf("%d, %d", a, b);
 
-	for (i = 1; i < 91; i++)
+	while (count < 98)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-	 bef1 = (bef / l);
-	 bef2 = (bef % l);
-	 aft1 = (aft / l);
-	 aft2 = (aft % l);
+		c = a + b;
+		printf(", ");
+		printf("%d", c);
 
-	for (i = 92; i < 99; i++)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		a = b;
+		b = c;
+
+		count++;
 	}
 	printf("\n");
 	return (0);

edf7ec465cd318bea5ef55397d081b49ecb57144
Damilola-2 <adedahmek00@gmail.com>
2023-08-21 11:48:00 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Print a string followed by a new line
+ * @str: input
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}

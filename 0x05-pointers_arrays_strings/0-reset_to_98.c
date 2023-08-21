a6d549ec584865bd35439a48f2404434ac7d5d8f
Damilola-2 <adedahmek00@gmail.com>
2023-08-21 11:25:47 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,18 +8,7 @@
  * @n: num
  * Return: 0
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
-
-	int n = 42;
-
-	printf("Before reset: %d\n", n);
-
-	reset_to_98(&n); /* pass a pointer to a num*/
-
-	printf("After reset: %d\n", n);
-
-	return (0);
 }

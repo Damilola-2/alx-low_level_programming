0902d0e5a79e41c957f5507693d55baafc2910ef
Damilola-2 <adedahmek00@gmail.com>
2023-08-21 15:00:58 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i])
+	while (*s[i])
 		i++;
 
 	while (i--)
 	{
-		_putchar(s[i]);
+		putchar(*s[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
